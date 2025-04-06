adb682d463fe2497ab9f4dbaa190996f82318593
Bhanuka Wickramasinghe <bhanuka@ensiz.com>
2025-04-06 15:07:41 +0530

    Add AWS Amplify integration and update dependencies
    - Updated .gitignore to include Amplify-related files.
    - Added AWS Amplify and UI React dependencies in package.json.
    - Modified App component to use withAuthenticator for authentication.
    - Configured Amplify in index.tsx with aws-exports.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cognitoauthappd7a6645d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+function App({ signOut, user }: any) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
